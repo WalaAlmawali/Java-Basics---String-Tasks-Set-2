a4d3be59518441629a3aec6e1091b608fc2a9ddd
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 16:20:14 +0400

    Create a function named plusOut, It will return a string value

--- src/PlusOut.java
+++ src/PlusOut.java
@@ -4,5 +4,22 @@ public class PlusOut {
 
 
     }
+    public static String plusOut(String str, String word) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); ) {
+            if (i <= str.length() - word.length() &&
+                    str.substring(i, i + word.length()).equals(word)) {
+
+                result.append(word);
+                i += word.length();
+            } else {
+                result.append('+');
+                i++;
+            }
+        }
+
+        return result.toString();
+    }
 
     }
