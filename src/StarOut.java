22535c59549f7ef857270e137962a237863ff922
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 16:16:40 +0400

    Create a function named starOut, It will return a string value

--- src/StarOut.java
+++ src/StarOut.java
@@ -4,5 +4,24 @@ public class StarOut {
 
 
     }
+    public static String starOut(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') {
+                continue;
+            }
+
+
+            if ((i > 0 && str.charAt(i - 1) == '*') ||
+                    (i < str.length() - 1 && str.charAt(i + 1) == '*')) {
+                continue;
+            }
+
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
+    }
 
     }
