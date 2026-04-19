49e6c928bf6e3792b7b6314a73fd8e8d57f953d3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 16:06:38 +0400

    Create a function named oneTwo, It will return  string value

--- src/oneTwo.java
+++ src/oneTwo.java
@@ -3,6 +3,19 @@ public class oneTwo {
 
 
 
+
+    }
+
+    public static String oneTwo(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i + 2 < str.length(); i += 3) {
+            result.append(str.charAt(i + 1));
+            result.append(str.charAt(i + 2));
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
     }
 
     }
