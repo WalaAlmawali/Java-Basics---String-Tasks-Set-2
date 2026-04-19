de29fa4978b1898e4054696aef7ee21fe8b55e7c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 11:54:01 +0400

    Check if xyz appears not immediately preceded by '.'+ call xyzThere function in the main method

--- src/XyzExample.java
+++ src/XyzExample.java
@@ -1,12 +1,19 @@
 public class XyzExample {
     public static void main(String[] args){
 
-
+        System.out.println(xyzThere("abc.xyz"));
 
     }
 
     public static boolean xyzThere(String str) {
+        if(str.contains("xyz")){
+            int startIndexOfXyz = str.indexOf("xyz");
+              if(str.charAt(startIndexOfXyz -1) != '.'){
+                  return true;
+              }
+        }
 
+        return false;
 
     }
 }
