4b8bc76c6d88faa53bc6e9c1bf2822d7ddac1169
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 15:50:56 +0400

    Call getSandwich in the main method

--- src/GetSandwich.java
+++ src/GetSandwich.java
@@ -1,6 +1,7 @@
 public class GetSandwich {
     public static void main(String[] args) {
 
+        System.out.println(getSandwich("breadjambread"));
 
     }
 
@@ -9,7 +10,7 @@ public class GetSandwich {
         int first = str.indexOf("bread");
         int last = str.lastIndexOf("bread");
 
-
-    }
+        return str.substring(first + 5, last);
+      }
 
     }
