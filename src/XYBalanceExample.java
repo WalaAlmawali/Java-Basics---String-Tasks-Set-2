a771d46892ab8d8833d46f0fbd3c9ba25fe0f4e9
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 12:12:42 +0400

    Add an if-else statement to check whether every 'x' has a 'y' somewhere later + call xyBalance function in the main method

--- src/XYBalanceExample.java
+++ src/XYBalanceExample.java
@@ -1,12 +1,24 @@
 public class XYBalanceExample {
     public static void main(String[] args) {
 
+        System.out.println(xyBalance("aaxbb"));
+
 
     }
 
     public static boolean xyBalance(String str) {
 
+        if(str.contains("x")){
+            int IndexOfX = str.indexOf("x");
 
+            for(int i = IndexOfX +1;i<str.length();i++ ){
+                if(str.charAt(i) == 'y'){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     }
