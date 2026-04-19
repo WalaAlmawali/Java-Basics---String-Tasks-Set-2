cf9b4ff71124048a2e19da2ec0d32a8a0e23b807
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 10:43:37 +0400

    create scanner object + ask the user to enter sample text+ Store the entered value in a String variable

--- src/StringDoubleChar.java
+++ src/StringDoubleChar.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class StringDoubleChar {
 
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sample string: ");
+        String text = sc.nextLine();
+
+
+
 
 
 
