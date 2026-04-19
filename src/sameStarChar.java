80fd0b4e8e529caee49f87770cc2f907da77c801
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 15:55:38 +0400

    Create a function named sameStarChar, It will return a boolean value

--- src/sameStarChar.java
+++ src/sameStarChar.java
@@ -4,5 +4,19 @@ public class sameStarChar {
 
 
     }
+    public static boolean sameStarChar(String str) {
 
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') {
+
+
+                if (str.charAt(i - 1) == str.charAt(i + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+
+
+}
