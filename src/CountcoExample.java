a45c3c263575f329057b6a7272039bd71c2b702b
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 11:32:50 +0400

    Check if the co?e pattern is present + update code value

--- src/CountcoExample.java
+++ src/CountcoExample.java
@@ -7,9 +7,14 @@ public class CountcoExample {
 
         System.out.print("Enter a sample string: ");
         String text = sc.nextLine();
+        int code= 0;
 
         for (int i = 0; i < text.length() -3; i++) {
 
+            if ((text.substring(i, i + 2).equals("co")) &&( text.charAt(i+3)=='e' )) {
+                code++;
+
+            }
 
         }
 
