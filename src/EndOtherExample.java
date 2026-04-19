c8e32d7a9842fcddc32bee0d106e96b550eea4e7
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 11:43:16 +0400

    Add an if-else statement to check whether  one string appears at the end of the other

--- src/EndOtherExample.java
+++ src/EndOtherExample.java
@@ -4,6 +4,11 @@ public class EndOtherExample {
         String str1 = "Hiabc";
         String str2 = "abc";
 
+        if (str1.endsWith(str2) || str2.endsWith(str1)) {
+             System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
 
 
     }
