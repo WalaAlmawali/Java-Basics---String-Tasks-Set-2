4fd2b8c0cdfa3b339807a36ce9086ad02279fbd8
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 15:36:00 +0400

    Create a function named prefixAgain it will take two argument and return a boolean value

--- src/prefixAgain.java
+++ src/prefixAgain.java
@@ -1,8 +1,23 @@
 public class prefixAgain {
     public static void main(String[] args) {
 
+        System.out.println(prefixAgain("abXYabc", 3));
 
 
     }
+
+
+    public static boolean prefixAgain(String str, int n) {
+        String prefix = str.substring(0, n);
+
+        for (int i = 1; i <= str.length() - n; i++) {
+            if (str.substring(i, i + n).equals(prefix)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
     }
