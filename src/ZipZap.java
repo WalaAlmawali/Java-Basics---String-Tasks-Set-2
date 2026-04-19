e36230a7756118b1edabff5f2329063ed8f014e4
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 16:11:29 +0400

    Create a function named zipZap, It will return a string value

--- src/ZipZap.java
+++ src/ZipZap.java
@@ -3,5 +3,24 @@ public class ZipZap {
 
 
     }
+    public static String zipZap(String str) {
+        StringBuilder result = new StringBuilder();
 
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i < str.length() - 2 &&
+                    str.charAt(i) == 'z' &&
+                    str.charAt(i + 2) == 'p') {
+
+                result.append("zp");
+                i += 2;
+            } else {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
     }
+
+
+}
