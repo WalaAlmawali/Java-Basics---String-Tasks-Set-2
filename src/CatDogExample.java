d09e2432284c6b53f4ee0572ef97259339110cc5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 11:13:41 +0400

    Create a for loop to go through each character of the string + Check each 3-character substring + updte catCount and dogCount

--- src/CatDogExample.java
+++ src/CatDogExample.java
@@ -2,12 +2,31 @@ public class CatDogExample {
     public static void main(String[] args){
 
 
+
     }
 
     public  boolean catDog(String str) {
 
+        int catCount = 0;
+        int dogCount = 0;
 
+        for (int i = 0; i < str.length() - 2; i++) {
 
+            String sub = str.substring(i, i + 3);
+
+            if (sub.equals("cat")) {
+                catCount++;
+            }
+
+            if (sub.equals("dog")) {
+                dogCount++;
+            }
+        }
+        if(catCount == dogCount){
+            return true;
+        }
+
+        return false;
     }
 
     }
