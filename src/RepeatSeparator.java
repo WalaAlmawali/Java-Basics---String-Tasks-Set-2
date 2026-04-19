7a8829fe6ad3714afd8ee3ced192477b654e7ac3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 15:22:33 +0400

    Create a function name repeatSeparator it will take three arguments and return a string value

--- src/RepeatSeparator.java
+++ src/RepeatSeparator.java
@@ -1,8 +1,23 @@
 public class RepeatSeparator {
     public static void main(String[] args) {
 
+        System.out.println(repeatSeparator("Word", "X", 3));
 
 
     }
+    public static String repeatSeparator(String word, String sep, int count) {
 
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            result.append(word);
+            if (i < count - 1) {
+                result.append(sep);
+            }
+        }
+
+        return result.toString();
     }
+
+
+}
