74138406328d26c3515f190b760eab5b20b12774
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 12:01:23 +0400

    Add an if-else statement to check whether b?b pattern present or not

--- src/BobThereExample.java
+++ src/BobThereExample.java
@@ -7,9 +7,13 @@ public class BobThereExample {
     public static boolean bobThere(String str) {
         for (int i = 0; i < str.length() -2; i++) {
 
+            if(str.charAt(i) == 'b' && str.charAt(i+2) == 'b'  ){
+                return true;
+            }
 
         }
 
+        return false;
 
     }
     }
