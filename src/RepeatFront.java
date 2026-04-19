f63c979911bb4092059138c7077ac4675f4727f3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 15:15:25 +0400

    Create a function named repeatFront and it will take two argument and return a string value

--- src/RepeatFront.java
+++ src/RepeatFront.java
@@ -2,7 +2,17 @@ public class RepeatFront {
     public static void main(String[] args) {
 
 
+        System.out.println((repeatFront("Zelda", 3)));
 
     }
 
+    public static String repeatFront(String str, int num) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = num; i >= 1; i--) {
+            result.append(str.substring(0, i));
+        }
+
+        return result.toString();
     }
+}
