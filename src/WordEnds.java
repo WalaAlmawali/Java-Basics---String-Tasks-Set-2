2fb09ea65bed7311a330ccd09b72405d7cd2fef2
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-19 16:24:10 +0400

    Create a function named wordEnds, It will return a string value

--- src/WordEnds.java
+++ src/WordEnds.java
@@ -5,5 +5,27 @@ public class WordEnds {
 
 
     }
+    public static String wordEnds(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        int len = word.length();
 
+        for (int i = 0; i <= str.length() - len; i++) {
+            if (str.substring(i, i + len).equals(word)) {
+
+
+                if (i > 0) {
+                    result.append(str.charAt(i - 1));
+                }
+
+
+                if (i + len < str.length()) {
+                    result.append(str.charAt(i + len));
+                }
+            }
+        }
+
+        return result.toString();
     }
+
+
+}
